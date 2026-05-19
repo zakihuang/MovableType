342e3a627cc249655e7188112ad10132cc61ae79
黄亮 <huangliang@avic-intl.cn>
2026-05-19 17:58:41 +0800

    加一版部署测试

--- .umirc.ts
+++ .umirc.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'umi';
 import path from 'path';
 
+const isDeploy = process.env.DEPLOY === '1';
+
 export default defineConfig({
   mode: 'doc',
   title: 'MovableType',
   description: '纯渲染型配置引擎 - JSON 即表单',
   locales: [['zh-CN', '中文']],
   outputPath: 'docs',
-  publicPath: '/MovableType/',
-  base: '/MovableType',
+  publicPath: isDeploy ? '/MovableType/' : '/',
+  base: isDeploy ? '/MovableType' : '/',
   routes: [
     { path: '/', component: './src/README.md', exact: true },
   ],
